327211df0500d42ea809658892381fd54f2d22d8
Daniel Griffiths <danielgriffiths@Daniels-MacBook-Pro-2.local>
2016-10-08 14:32:39 -0400

    danupdateshit

--- routes.rb
+++ routes.rb
@@ -43,7 +43,7 @@ delete '/api/movies/:title' do |title|
 end
 
 #post rating through user with movie_id>>movie.id(title) !!!WORKING!!!
-post '/api/ratings' do
+post '/api/ratings/post' do
   rating = Rating.new(movie_id: params[:movie_id], user_id: params[:user_id], score: params[:score])
   if rating.valid? && rating.save
     status 201
@@ -53,120 +53,33 @@ post '/api/ratings' do
   end
 end
 
-#get all ratings for user_id !!!WORKING!!!     INCLUDe?????
-get '/api/ratings/:user_id' do |user_id|
- ratings = Rating.where(user_id: user_id)
+#get all ratings for user_id !!!WORKING!!!
+get '/api/ratings/for/user/:user_id' do |user_id|
+ ratings = Rating.all.where(user_id: user_id)
  ratings.to_json
 end
 
-#delete all reviews from user
-delete '/api/ratings/:user_id' do |user_id|
-  ratings = Rating.find_by(user_id: user_id)
+#get all ratings for user_id where rating is 5 !!!WORKING!!!
+get '/api/ratings/top/:user_id' do |user_id|
+ ratings = Rating.all.where(user_id: user_id).where(score: 5)
+ ratings.to_json
+end
+
+#get 5, 5 star ratings for movie_id !!!WORKING!!!
+get '/api/ratings/movie/top/:movie_id' do |movie_id|
+ ratings = Rating.all.where(movie_id: movie_id).where(score: 5).limit(5)
+ ratings.to_json
+end
+
+#get average rating for movie id
+get '/api/ratings/average/:movie_id' do |movie_id|
+ ratings = Rating.where(movie_id: movie_id).average(score: score)
+ ratings.to_json
+end
+
+#delete all reviews from user !!!PROBS WORKING NOT GONNA TEST LOL!!!
+delete '/api/ratings/delete/:user_id' do |user_id|
+  ratings = Rating.all.where(user_id: user_id)
   ratings.destroy
   nil
 end
-
-
-
-
-
-
-# after do
-#   ActiveRecord::Base.connection.close
-# end
-#
-# get '/api/composers' do
-#   composers = Composer.all
-#   composers.to_json
-# end
-#
-# post '/api/composers' do
-#   composer = Composer.new(name: params[:name], period: params[:period])
-#   if composer.valid? && composer.save
-#     status 201
-#     composer.to_json
-#   else
-#     status 400
-#   end
-# end
-#
-# get '/api/composers/:id' do |id|
-#   composer = Composer.find_by(id: id)
-#   composer.to_json
-# end
-#
-# put '/api/composers/:id' do
-#   id = params['id']
-#   name = params['name']
-#   period = params['period']
-#   composer = Composer.find_by(id: id)
-#   composer.update(name: name, period: period)
-#   composer.to_json
-# end
-#
-# delete '/api/composers/:id' do
-#   id = params['id']
-#   composer = Composer.find_by(id: id)
-#   composer.destroy
-#   nil
-# end
-# #put '/api/composers/:id' do
-#
-# #end
-#
-# #delete '/api/composers/:id' do
-#
-# #end
-#
-#
-#
-#
-# get '/api/pieces' do
-#   pieces = Piece.all
-#   status 200
-#   pieces.to_json
-# end
-#
-# post '/api/pieces' do
-#   piece = Piece.new(name: params[:name], composer_id: params[:composer_id])
-#   if piece.valid? && piece.save
-#     status 201
-#     piece.to_json
-#   else
-#     status 400
-#   end
-# end
-#
-# get '/api/pieces/:id' do |id|
-#   piece = Piece.find_by(id: id)
-#   piece.to_json
-# end
-#
-# put '/api/pieces/:id' do
-#   id = params['id']
-#   name = params['name']
-#   composer_id = params['composer_id']
-#   piece = Piece.find_by(id: id)
-#   piece.update(name: name, composer_id: composer_id)
-#   piece.to_json
-# end
-#
-# delete '/api/pieces/:id' do
-#   id = params['id']
-#   piece = Piece.find_by(id: id)
-#   piece.destroy
-#   nil
-# end
-# #Piece.order(name: something)
-# #put '/api/pieces/:id' do
-# #piece.delete
-# #end
-# get '/api/composers_pieces' do
-#   composers = Composer.all
-#   pieces = Piece.all
-#   composers + pieces.to_json
-# end
-#
-# #delete '/api/pieces/:id' do
-#
-# #end
