faef10d58347b05513e2efbca1b45c7a7e8496d8
Daniel Griffiths <danielgriffiths@Daniels-MacBook-Pro-2.local>
2016-10-10 09:48:09 -0400

    clean_final_double_function

--- routes.rb
+++ routes.rb
@@ -36,7 +36,7 @@ get '/api/get/movie/:title' do |title|
 
   rating = Rating.select(:score).where(movie_id: movie_info[:id]).average(:score)
 
-  top_users = Rating.all.where(movie_id: movie_info[:id]).where(score: 5).limit(5)
+  top_users = Rating.select(:user_id).where(movie_id: movie_info[:id]).where(score: 5).limit(5)
 
   payload = {'movie_info' => movie_info, 'rating' => rating, 'top_users' => top_users}
   payload.to_json
@@ -46,7 +46,6 @@ get '/api/twenty/movies' do
   movie = Movie.limit(20)
   movie.to_json
 end
-
 
 post '/api/post/ratings/post' do
   rating = Rating.new(movie_id: params[:movie_id], user_id: params[:user_id], score: params[:score])
@@ -59,17 +58,30 @@ post '/api/post/ratings/post' do
 end
 
 get '/api/ratings/of/user/:user_id' do |user_id|
-  ratings = Rating.all.where(user_id: user_id)
+  ratings = Rating.select(:movie_id).where(user_id: user_id)
   ratings.to_json
 end
 
 get '/api/ratings/top/:user_id' do |user_id|
-  ratings = Rating.all.where(user_id: user_id).where(score: 5)
-  ratings.to_json
+  top_ratings = []
+
+  ratings = Rating.select(:movie_id, :score).where(user_id: user_id).where(score: 5)
+
+  ratings.each do |rating|
+    movie = Movie.find_by_id(rating.movie_id)
+
+    top_ratings << {
+      score: rating.score,
+      movie_id: rating.movie_id,
+      movie_title: movie.title
+    }
+  end
+
+  top_ratings.to_json
 end
 
 get '/api/ratings/top/five/:movie_id' do |movie_id|
-  ratings = Rating.all.where(movie_id: movie_id).where(score: 5).limit(5)
+  ratings = Rating.select(:user_id).where(movie_id: movie_id).where(score: 5).limit(5)
   ratings.to_json
 end
 
